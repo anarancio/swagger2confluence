dd5b4778224206e82c99f802d32f010514a033b3
Alejandro Narancio <ale.narancio@gmail.com>
2016-02-25 00:20:55 -0500

    first commit

---
+++ src/main/java/com/narancio/swagger2confluence/App.java
@@ -0,0 +1,13 @@
+package com.narancio.swagger2confluence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
